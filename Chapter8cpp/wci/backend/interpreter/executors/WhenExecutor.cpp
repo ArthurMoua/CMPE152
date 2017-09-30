8a724d1dc5dc292f3c2a0e363648aa61d62f51c9
Emilyjin <emilyzheng2009@gmail.com>
2017-09-29 20:38:40 -0700

    Add files via upload

--- Chapter8cpp/wci/backend/interpreter/executors/WhenExecutor.cpp
+++ Chapter8cpp/wci/backend/interpreter/executors/WhenExecutor.cpp
@@ -3,8 +3,64 @@
  *
  *  Created on: Sep 20, 2017
  *      Author: arthur moua
+
+
  */
 
 
 
+#include <vector>
+#include "WhenExecutor.h"
+#include "StatementExecutor.h"
+#include "ExpressionExecutor.h"
+#include "../../../DataValue.h"
+#include "../../../intermediate/ICodeNode.h"
+#include "../../../intermediate/icodeimpl/ICodeNodeImpl.h"
+
+namespace wci { namespace backend { namespace interpreter { namespace executors {
+
+using namespace std;
+using namespace wci;
+using namespace wci::backend::interpreter;
+using namespace wci::intermediate;
+using namespace wci::intermediate::icodeimpl;
+
+WhenExecutor::WhenExecutor(Executor *parent)
+    : StatementExecutor(parent)
+{
+}
+
+DataValue *WhenExecutor::execute(ICodeNode *node)
+{
+    // Get the IF node's children.
+    vector<ICodeNode *> children = node->get_children();
+    ICodeNode *expr_node = children[0];
+    ICodeNode *right_arrow_stmt_node = children[1];
+    ICodeNode *next_expr_node = children[2];
+ //   ICodeNode *otherwise_stmt_node = children.size > 2 ? children[3] : nullptr;
+
+    ExpressionExecutor expression_executor(this);
+    StatementExecutor statement_executor(this);
+
+    // Evaluate the boolean expression to determine which statement subtree child to execute.
+    DataValue *data_value = expression_executor.execute(expr_node);
+    //create another dtate value to evaluate the boolean expr.
+    if (data_value->b)
+    {
+        statement_executor.execute(right_arrow_stmt_node);
+    }
+    else if (next_expr_node != nullptr)
+    {
+        statement_executor.execute(right_arrow_stmt_node);
+    }
+ //   else if (otherwise_stmt_node != nullptr)
+   // {
+  /// 	statement_executor.execute(otherwise_stmt_node);
+   // }
+
+    ++execution_count;  // count the WHEN statement itself
+    return nullptr;
+}
+
+}}}}  // namespace wci::backend::interpreter::executors
 
--- Chapter8cpp/wci/backend/interpreter/executors/WhenExecutor.h
+++ Chapter8cpp/wci/backend/interpreter/executors/WhenExecutor.h
@@ -5,11 +5,38 @@
  *      Author: arthur moua
  */
 
-#ifndef WCI_BACKEND_INTERPRETER_EXECUTORS_WHENEXECUTOR_H_
-#define WCI_BACKEND_INTERPRETER_EXECUTORS_WHENEXECUTOR_H_
+#ifndef WHENEXECUTOR_H_
+#define WHENEXECUTOR_H_
 
+#include "StatementExecutor.h"
+#include "../../../DataValue.h"
+#include "../../../intermediate/ICodeNode.h"
 
+namespace wci { namespace backend { namespace interpreter { namespace executors {
 
+using namespace std;
+using namespace wci;
+using namespace wci::backend::interpreter;
+using namespace wci::intermediate;
 
+class WhenExecutor : public StatementExecutor
+{
+public:
+    /**
+     * Constructor.
+     * @param the parent executor.
+     */
+    WhenExecutor(Executor *parent);
 
-#endif /* WCI_BACKEND_INTERPRETER_EXECUTORS_WHENEXECUTOR_H_ */
+    /**
+     * Execute a compound statement.
+     * @param node the root node of the compound statement.
+     * @return nullptr.
+     */
+    DataValue *execute(ICodeNode *node);
+};
+
+}}}}  // namespace wci::backend::interpreter::executors
+
+#endif /* WHENEXECUTOR_H_ */
+
